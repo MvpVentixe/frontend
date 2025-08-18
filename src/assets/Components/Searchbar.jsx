92504f7bea5c9ab6cae57a38a1e9fd1dbf2a70c8
Ernesto Linares <ernestol92@hotmail.com>
2025-08-18 14:35:24 -0300

    adjusted correct api call url

--- src/assets/Components/Searchbar.jsx
+++ src/assets/Components/Searchbar.jsx
@@ -3,11 +3,13 @@ import "../Css/Searchbar.css"
 
 const Searchbar = ( {OnSearch} ) => {
 
+  const [value, setValue] = useState("")
+
   const handleSubmit = async (e)=>{
     e.preventDefault();
     try {
-
-      const response = await fetch("url");
+      // then paste value variable into the url
+      const response = await fetch(`https://eventserviceapplication-etgsccg0b3fhhbcv.swedencentral-01.azurewebsites.net/api/Event/onsearch/${value}`);
       if(!response.ok){
         const errordata = await response.json();
         alert(errordata.message || "failed to load search results")
@@ -15,6 +17,7 @@ const Searchbar = ( {OnSearch} ) => {
       }
 
       const data = await response.json();
+      OnSearch(data)
       
     } catch (error) {
       
@@ -24,7 +27,7 @@ const Searchbar = ( {OnSearch} ) => {
   return (
     <div>
         <form action="/search" onSubmit={handleSubmit} className='searchbar'>
-            <input type="text" placeholder='Search name, event, etc' className='search-input'/>
+            <input type="text" placeholder='Search name, event, etc' className='search-input' onChange={(e) => setValue(e.target.value)}/>
             <button type="submit" className='search-btn'><img src="/Icons/System/MagnifyingGlass.svg" alt="" className='search-btn-icon' /></button>
       </form>
     </div>
